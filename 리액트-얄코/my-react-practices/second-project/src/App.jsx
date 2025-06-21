59607bd3b63adfa423a11a29663bfca8e7cb5f31
1028ragon <1028ragon@naver.com>
2025-06-22 03:38:15 +0900

    .

--- 리액트-얄코/my-react-practices/second-project/src/App.jsx
+++ 리액트-얄코/my-react-practices/second-project/src/App.jsx
@@ -1,10 +1,57 @@
 import './App.css'
 
+const element1 = <h2>Hello, World!</h2>
+const element2 = (
+  <ul>
+    <li>A</li>
+    <li>B</li>
+    <li>C</li>
+  </ul>
+)
+
+function ElementExpressions() {
+  return (
+    <section>
+      {element1}
+      {element2}
+    </section>
+  )
+}
+
+
+// 중괄호 표현식
+
+const language = "JavaScript"
+
+// 문법
+
+function BasicExpressions() {
+  const name = "John";
+  const age = 25;
+  const isAdmin = true;
+
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Age next year: {age + 1}</p>
+      <p>{name + "'s Profile"}</p>
+      <p>{`${name} is ${age} years old`}</p>
+      <p>Admin Status: {String(isAdmin)}</p>
+    </div>
+  )
+}
+
+// 웹사이트에 표시시
+
 function App() {
+
   return (
     <>
-      {/* JSX Practices */}
       <h1>JSX</h1>
+      <BasicExpressions />
+      <ElementExpressions />
+
+      {language}
     </>
   )
 }
