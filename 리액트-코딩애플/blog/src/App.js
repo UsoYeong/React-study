6b8b44b791228a96e50bf2f4d203f45bf386e2f0
1028ragon <1028ragon@naver.com>
2025-06-23 00:03:40 +0900

    ~component

--- 리액트-코딩애플/blog/src/App.js
+++ 리액트-코딩애플/blog/src/App.js
@@ -6,23 +6,25 @@ import { useState } from 'react';
 
 function App() {
 
-let post = '여기에 포스트 쓰면 됨';
 let [logo, setLogo] = useState('ReactBlog');
 let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
 let [따봉, 따봉변경] = useState(0);
-let [여자코트, 여자코트변경] = useState('여자 코트 추천')
-
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{color : 'red', fontSize : '16px'}}>{ logo }</h4>
       </div>
 
-      <button onClick={ ()=>{
+      <button onClick={ function() {
         let copy = [...글제목];
-        copy[0] = '여자 코트 추천';
+        copy[0] = '여자코트 추천';
+        글제목변경(copy)} }>여자로 변경</button>
+
+      <button onClick={ function() {
+        let copy = [...글제목]
+        copy.sort();
         글제목변경(copy)
-      }}>글수정</button>
+      }}>가나다순 정렬</button>
 
       <div className='list'>
         <h4>{ 글제목[0] } 
@@ -38,9 +40,18 @@ let [여자코트, 여자코트변경] = useState('여자 코트 추천')
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
-      <h4>{post}</h4>
     </div>
   );
+}
+
+function Modal() {
+  return (
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>    
+  )
 }
 
 export default App;
