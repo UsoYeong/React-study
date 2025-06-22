0a09a79d63d59e03a439af76239742ac83550ae8
1028ragon <1028ragon@naver.com>
2025-06-23 00:03:40 +0900

    ~div 줄이기

--- 리액트-코딩애플/blog/src/App.js
+++ 리액트-코딩애플/blog/src/App.js
@@ -6,11 +6,11 @@ import { useState } from 'react';
 
 function App() {
 
-let post = '여기에 포스트 쓰면 됨';
 let [logo, setLogo] = useState('ReactBlog');
 let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-let [따봉, 따봉변경] = useState(0);
-let [여자코트, 여자코트변경] = useState('여자 코트 추천')
+let [따봉, 따봉변경] = useState([0, 0, 0]);
+
+let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -18,29 +18,75 @@ let [여자코트, 여자코트변경] = useState('여자 코트 추천')
         <h4 style={{color : 'red', fontSize : '16px'}}>{ logo }</h4>
       </div>
 
-      <button onClick={ ()=>{
+      <button onClick={ function() {
         let copy = [...글제목];
-        copy[0] = '여자 코트 추천';
-        글제목변경(copy)
-      }}>글수정</button>
+        copy[0] = '여자코트 추천';
+        글제목변경(copy)} }>여자로 변경</button>
 
-      <div className='list'>
+      <button onClick={ function() {
+        let copy = [...글제목]
+        copy.sort();
+        글제목변경(copy)
+      }}>가나다순 정렬</button>
+
+      {/* <div className='list'>
         <h4>{ 글제목[0] } 
           <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} 
         </h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className='list'>
         <h4>{ 글제목[1] }</h4>
+        <button onClick={ function() { setModal(!modal) }}>창 열기</button>
         <p>2월 17일 발행</p>
+        { // false : 기본값, 안보임
+        modal == true ? <Modal/> : null
+      }
+        
       </div>
       <div className='list'>
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
-      </div>
-      <h4>{post}</h4>
+      </div> */}
+
+      {
+        글제목.map(function(a, i) {
+          return (
+            <div className='list'>
+              <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[i] } 
+                <span onClick={()=>{ // state가 array자료일땐 복사부터 하고 그거 수정
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1
+                  따봉변경(copy)
+                  }}>👍</span> {따봉[i]} </h4>
+              <p>2월 17일 발행</p>
+            </div>
+
+          )
+        })
+      }
+
+{ // false : 기본값, 안보임
+        modal == true ? <Modal/> : null
+}
+        
     </div>
   );
+}
+
+
+
+function Modal() {
+  return (
+    <>
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div> 
+    </>   
+  )
 }
 
 export default App;
