3af026ab7f6e836fe585ab79f75ee95dc033e236
1028ragon <1028ragon@naver.com>
2025-06-23 18:16:01 +0900

    커밋임

--- 리액트-코딩애플/blog/src/App.js
+++ 리액트-코딩애플/blog/src/App.js
@@ -6,11 +6,11 @@ import { useState } from 'react';
 
 function App() {
 
-let [logo, setLogo] = useState('ReactBlog');
-let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [logo, setLogo] = useState('ReactBlog');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
 
-let [modal, setModal] = useState(false);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
