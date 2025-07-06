dad27f0f101db9885ddf161a73f97101f4be0812
1028ragon <1028ragon@naver.com>
2025-07-06 20:17:07 +0900

    add page

--- 리액트-코딩애플/shop/src/App.js
+++ 리액트-코딩애플/shop/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import { Navbar, Container, Nav, Col, Row } from 'react-bootstrap';
+import data from './data.js'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
+
+  let [items] = useState(data)
+
   return (
     <div className="App">
 
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">SSOZERO Shop</Navbar.Brand>
+          <Navbar.Brand href="#home">유소영 Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Cart</Nav.Link>
@@ -16,8 +22,80 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
+
+
+      <Routes>
+        <Route path='/' element={
+          <>
+           <div className='main-bg'></div>
+          <Container>
+           <Row>
+            <Card items={items}></Card>
+            <Card2 items={items}></Card2>
+           <Card3 items={items}></Card3>
+          </Row>
+        </Container>
+        </>
+        } />
+        <Route path='/detail' element={
+          <>
+            <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <img src="https://cdn.pixabay.com/photo/2021/02/02/18/46/city-5974876_1280.jpg" width="80%" />
+             </div>
+              <div className="col-md-6">
+                <h4 className="pt-5">상품명</h4>
+                <p>상품설명</p>
+                <p>120000원</p>
+               <button className="btn btn-danger">주문하기</button> 
+             </div>
+            </div>
+          </div> 
+          </>} />
+      </Routes>
+
     </div>
   );
+}
+
+
+
+
+function Card(props) {
+  return(
+    <Col>
+      <img src='https://cdn.pixabay.com/photo/2021/02/02/18/46/city-5974876_1280.jpg' width="80%"/>
+      <h4>{props.items[0].title}</h4>
+      <p>{props.items[0].content}</p>
+      <p>{props.items[0].price}</p>
+    </Col>
+  )
+}
+
+function Card2(props) {
+  return(
+    <Col>
+      <img src='https://cdn.pixabay.com/photo/2018/02/18/13/03/cute-3162209_1280.jpg' width="80%"/>
+      <h4>{props.items[1].title}</h4>
+      <p>{props.items[1].content}</p>
+      <p>{props.items[1].price}</p>
+    </Col>  
+  )
+}
+
+function Card3(props) {
+  return(
+    <Col>
+      <img src='https://cdn.kiranico.net/file/kiranico/mhworld-web/images/wp/factory/saxe/RxU8uPN.jpg' width="80%"/>
+      <h4>{props.items[2].title}</h4>
+      <p>{props.items[2].content}</p>
+      <p>{props.items[2].price}</p>
+    </Col>
+  )
 }
 
 export default App;
---
+++ 리액트-코딩애플/shop/src/data.js
@@ -0,0 +1,23 @@
+let data = [
+  {
+    id : 0,
+    title : "반포 자이",
+    content : "개비쌈",
+    price : "3520억 8000만원"
+  },
+
+  {
+    id : 1,
+    title : "마법의 햄스터",
+    content : "귀여움",
+    price : "5억 5500만원"
+  },
+
+  {
+    id : 2,
+    title : "몬헌 슬래시액스 아룡검부[식열]",
+    content : "생산소재 : 중용골 x3, 적룡의 억센발톱 x3, 강인한 발톱 x3",
+    price : "생산금액 : 24,000z"
+  }
+]
+ export default data;
